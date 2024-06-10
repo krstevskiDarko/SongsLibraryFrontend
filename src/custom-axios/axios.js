f07c5d9171ffc62851d5158b592de21cdfde0238
krstevskiDarko <darko_krstevski@icloud.com>
2024-06-10 22:48:59 +0200

    Changed backend service port

--- src/custom-axios/axios.js
+++ src/custom-axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080/api/',
+    baseURL: 'https://songslibrary-songslibrary.azuremicroservices.io/api/',
     headers : {
         'Access-Control-Allow-Origin': '*',
     }
